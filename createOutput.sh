c76e0a733a8bd00736bc0a07024a35855ca0256b
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2015-08-08 07:10:28 +0200

    createOutput.sh: Add newlines after commas in output

--- createOutput.sh
+++ createOutput.sh
@@ -2,4 +2,6 @@
 # Using stderr for the actual output, and stdout for diagnostics, is backward,
 # but that's what SBT offers. At the same time, actual errors will go in the
 # file.
-sbt run 2> output.txt
+exec 3>&1
+sbt run 2>&1 1>&3 |sed -e 's/,/,\
+/g' > output.txt
